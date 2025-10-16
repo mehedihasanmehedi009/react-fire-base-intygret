318a41736a0402e1e311fce1ba792dbfdbe97df4
mehedihasanmehedi009 <mehedihasanmehedi007008@gmail.com>
2025-10-15 20:20:36 -0600

    mehedi

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,14 +1,29 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../auth.init";
 import { AuthContext } from "../AuthContext/AuthContext";
 
 const AuthProvider = ({ children }) => {
- 
+  
+    // registar
  const createUser = (email,password)=>{
     return createUserWithEmailAndPassword(auth,email,password)
  }
+
+const Singin = (email,password) =>{
+    return createUserWithEmailAndPassword(auth,email,password)
+}
+onAuthStateChanged(auth,(corruntuser)=>{
+ if(corruntuser){
+ console.log(corruntuser)
+ }
+ else{
+   console.log(corruntuser)
+ }
+})
+
  const Man = {
-    createUser
+    createUser,
+    Singin
  }
 
   return <AuthContext value={Man}>{children}</AuthContext>;
--- src/componentes/Layout/LogIn/Login.jsx
+++ src/componentes/Layout/LogIn/Login.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { use } from "react";
 import { Link } from "react-router";
+import { AuthContext } from "../../../AuthContext/AuthContext";
 
 const Login = () => {
+   const { Singin} = use(AuthContext)
+   const hendel = e =>{
+    e.preventDefault();
+    const email = e.target.email.value
+    const password= e.target.password.value
+    Singin(email,password)
+    .then(res=>{
+      console.log(res.user)
+    })
+    .catch(error=>{
+      console.log(error)
+    })
+     
+   }
+
   return (
     <div className="card bg-base-100 w-full max-w-sm mx-auto shrink-0 shadow-2xl">
       <div className="card-body">
         <h1 className="text-5xl font-bold">Please Registar</h1>
-        <form>
+        <form onSubmit={hendel}>
           <fieldset className="fieldset">
             <label className="label">Email</label>
             <input
